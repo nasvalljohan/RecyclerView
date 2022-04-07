0cec3c246f3cf37ae2c86eedbfbc9674347cef1f
nasvalljohan <j.nasvall95@gmail.com>
2022-04-07 12:45:06 +0200

    Added searchbar +1
    +Fixed doubleclick issue on remove

--- app/src/main/java/com/jojjenator/recyclerview/CustomAdapter.kt
+++ app/src/main/java/com/jojjenator/recyclerview/CustomAdapter.kt
@@ -1,30 +1,73 @@
 package com.jojjenator.recyclerview
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
-
-internal class CustomAdapter(private var itemsList: List<String>) : //kan vara annan arrayList
+internal class CustomAdapter(private var itemsList: ArrayList<String>) :
     RecyclerView.Adapter<CustomAdapter.MyViewHolder>(){
 
     // Init - Extend interface
     private lateinit var mListener : OnItemClickListener
+    var itemFilterList = ArrayList<String>()
+
+    // ???
+    init {
+        itemFilterList = itemsList
+    }
 
     // Create interface
     interface OnItemClickListener{
         fun onItemClick(position: Int)
     }
 
+    fun getFilter() : Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    itemFilterList = itemsList
+                } else {
+                    val resultList = ArrayList<String>()
+                    for (row in itemsList) {
+                        if (row.lowercase(Locale.ROOT).contains(charSearch.lowercase(Locale.ROOT))) {
+                            resultList.add(row)
+                        }
+                    }
+                    itemFilterList = resultList
+                }
+
+                // Is a must - For filtering
+                val filterResults = FilterResults()
+                filterResults.values = itemFilterList
+                return filterResults
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                if (results != null) {
+                    itemFilterList = results.values as ArrayList<String>
+                    notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+
+
     // Method Definition
-    fun setOnItemClickListener(listener: OnItemClickListener){
+    fun setOnItemClickListener(listener: OnItemClickListener) {
         mListener = listener
     }
 
-    internal inner class MyViewHolder (view: View, listener: OnItemClickListener) : RecyclerView.ViewHolder(view){
-        var itemTextView: TextView = view.findViewById(R.id.itemTextView) //kan vara annat ID
+    internal inner class MyViewHolder(view: View, listener: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+        var itemTextView: TextView = view.findViewById(R.id.itemTextView)
 
         init {
             itemView.setOnClickListener{
@@ -40,13 +83,12 @@ internal class CustomAdapter(private var itemsList: List<String>) : //kan vara a
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val item = itemsList[position] //kan vara annan arrayList
-        holder.itemTextView.text = item //kan vara annat ID
+        val item = itemsList[position]
+        holder.itemTextView.text = item
     }
 
     override fun getItemCount(): Int {
-        return itemsList.size // kan vara annan arrayList
+        // return itemsList.size
+        return itemFilterList.size
     }
-
-
 }
--- app/src/main/java/com/jojjenator/recyclerview/MainActivity.kt
+++ app/src/main/java/com/jojjenator/recyclerview/MainActivity.kt
@@ -2,40 +2,56 @@ package com.jojjenator.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-
 class MainActivity : AppCompatActivity() {
 
-    private val itemsList =  ArrayList<String>()
+    private val itemsList = ArrayList<String>()
     private lateinit var customAdapter: CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView) //vår view
+        // X()
+
+        val searchView = findViewById<androidx.appcompat.widget.SearchView>(R.id.sv_searchbar)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         customAdapter = CustomAdapter(itemsList)
         val layoutManager = LinearLayoutManager(applicationContext)
+
+        searchView.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                customAdapter.getFilter().filter(newText)
+                return false
+            }
+        })
 
         // NEW - Interface (on Item Click)
         customAdapter.setOnItemClickListener(object: CustomAdapter.OnItemClickListener{
             override fun onItemClick(position: Int) {
-                itemsList.removeAt(position)
-                customAdapter.notifyItemRemoved(position)
+
+                // CRASHES if it's -1
+                if (position >= 0) {
+                    itemsList.removeAt(position)
+                    customAdapter.notifyItemRemoved(position)
+                }
             }
         })
-
 
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = customAdapter
         prepareItems()
-
     }
 
     private fun prepareItems(){
-        for (index in 0..10){
+        for (index in 0..20){
             itemsList.add("Apples$index")
         }
         customAdapter.notifyDataSetChanged()
